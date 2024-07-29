69da0918a624f5dc827d964468ac0e2c3941f540
sans <simonesansalone777@gmail.com>
2024-07-30 01:11:46 +0200

    add selenium automation

--- ts/selenium_scripts/crea_pratiche_e_associa_anagrafiche_.py
+++ ts/selenium_scripts/crea_pratiche_e_associa_anagrafiche_.py
@@ -5,9 +5,7 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
 import time
-from selenium.webdriver.support.ui import Select
 
-d_t = 1
 
 def login_to_site(driver, username, password):
     # Navigate to the login page
@@ -26,7 +24,7 @@ def login_to_site(driver, username, password):
     login_button.click()
 
     # Wait for the page to load
-    time.sleep(5*d_t)
+    time.sleep(5)
 
 
 def navigate_to_pratiche(driver):
@@ -35,7 +33,7 @@ def navigate_to_pratiche(driver):
     pratiche_link.click()
 
     # Wait for the page to load
-    time.sleep(5*d_t)
+    time.sleep(5)
 
 
 def create_pratica(driver, denominazione, dati_pratica):
@@ -45,14 +43,10 @@ def create_pratica(driver, denominazione, dati_pratica):
     new_pratica_button.click()
 
     # Wait for the popup to load
-    time.sleep(3*d_t)
+    time.sleep(3)
 
     # Fill in the form fields with data from the JSON
-    #driver.find_element(By.ID, "tipologiapratica").send_keys(dati_pratica["tipologia"])
-    # driver.send_keys(dati_pratica["tipologia"])
-    select = Select(driver.find_element(By.ID, "tipologiapratica"))
-    select.select_by_value(dati_pratica["tipologia"])
-
+    driver.find_element(By.ID, "tipologiapratica").send_keys(dati_pratica["tipologia"])
     driver.find_element(By.ID, "nomepratica").send_keys(dati_pratica["nome_pratica"])
 
     # Ensure the "descrizione" field is interactable
@@ -65,26 +59,8 @@ def create_pratica(driver, denominazione, dati_pratica):
     driver.find_element(By.ID, "searchOggettoPratica").send_keys(dati_pratica["oggetto"])
     driver.find_element(By.ID, "avvocato").send_keys(dati_pratica["avvocato"])
     driver.find_element(By.ID, "statopratica").send_keys(dati_pratica["stato"])
-
-    ####################################################################################################################
     #driver.find_element(By.ID, "dataapertura").send_keys(dati_pratica["data_apertura"])
-
-    time.sleep(1*d_t)
-    date_input = driver.find_element(By.ID, 'dataapertura')
-    time.sleep(1*d_t)
-    # Rimuovi l'attributo onfocus per permettere l'input manuale
-    driver.execute_script("arguments[0].removeAttribute('onfocus')", date_input)
-    time.sleep(1*d_t)
-    # Imposta la data
-    date_input.clear()
-    time.sleep(1*d_t)
-    date_input.send_keys(dati_pratica["data_apertura"])
-    time.sleep(1*d_t)
-    ####################################################################################################################
-
     driver.find_element(By.ID, "tipoValore").send_keys(dati_pratica["tipo_valore"])
-    driver.find_element(By.ID, "valore").clear()
-    time.sleep(1*d_t)
     driver.find_element(By.ID, "valore").send_keys(dati_pratica["valore"])
 
     # Click the "Conferma" button
@@ -92,22 +68,21 @@ def create_pratica(driver, denominazione, dati_pratica):
     conferma_button.click()
 
     # Wait for the next page to load
-    time.sleep(5*d_t)
+    time.sleep(5)
 
     # Click on the "Soggetti" tab
     soggetti_tab = driver.find_element(By.XPATH, "//a[@href='#tabs-2-archive' and contains(@class, 'text-menu')]")
     soggetti_tab.click()
 
     # Wait for the page to load
-    time.sleep(5*d_t)
+    time.sleep(5)
 
-    ####################################################################################################################
     # Click on the "Seleziona Anagrafiche" button
     seleziona_anagrafiche_button = driver.find_element(By.ID, "newPartButton")
     seleziona_anagrafiche_button.click()
 
     # Wait for the popup to load
-    time.sleep(3*d_t)
+    time.sleep(3)
 
     # Switch to the iframe containing the search form
     driver.switch_to.frame(driver.find_element(By.ID, "addPartsFrame"))
@@ -118,103 +93,42 @@ def create_pratica(driver, denominazione, dati_pratica):
     popup_search_field.send_keys(denominazione)
 
     # Wait for the search results to load
-    time.sleep(3*d_t)
+    time.sleep(5)
 
     # Select the correct row based on denominazione
     rows = driver.find_elements(By.XPATH, "//tr[contains(@class, 'soggetti-row')]")
     for row in rows:
-        #print("°"*100)
-        #print(row)
+        print("°"*100)
+        print(row)
         cell = row.find_element(By.XPATH, ".//td[2]/span")
-        #print(cell.get_attribute("title"), denominazione)
+        print(cell.get_attribute("title"), denominazione)
         if cell.get_attribute("title") == denominazione:
             row.click()
             break
 
     # Wait for the popup to open
-    time.sleep(3*d_t)
+    time.sleep(3)
 
     # Switch to the newly opened modal (if necessary)
     driver.switch_to.default_content()  # Switch back to default content
     modal_iframe = driver.find_element(By.CSS_SELECTOR, "iframe[src*='frameindex']")
     driver.switch_to.frame(modal_iframe)
 
-    time.sleep(2*d_t)
+    time.sleep(2)
 
     # Select "Controparte" for both dropdowns
     person_role_select = driver.find_element(By.ID, "personRole")
-    time.sleep(1 * d_t)
     person_role_select.send_keys("Controparte")
 
     relazione_select = driver.find_element(By.ID, "relazione")
-    time.sleep(1 * d_t)
     relazione_select.send_keys("Controparte")
 
     # Click the "Aggiungi" button
     add_button = driver.find_element(By.ID, "addPartButton")
     add_button.click()
-    ####################################################################################################################
-    driver.switch_to.default_content()
-    time.sleep(3)
-    #input("...")
-    ####################################################################################################################
-    seleziona_anagrafiche_button = driver.find_element(By.ID, "newPartButton")
-    seleziona_anagrafiche_button.click()
 
-    # Wait for the popup to load
-    time.sleep(3*d_t)
-
-    # Switch to the iframe containing the search form
-    driver.switch_to.frame(driver.find_element(By.ID, "addPartsFrame"))
-
-    # Insert the search term in the search field in the iframe
-    popup_search_field = driver.find_element(By.ID, "searchField")
-    popup_search_field.clear()
-    time.sleep(1 * d_t)
-    popup_search_field.send_keys("SEV SPA")
-
-    # Wait for the search results to load
-    time.sleep(3*d_t)
-
-    # Select the correct row based on denominazione
-    rows = driver.find_elements(By.XPATH, "//tr[contains(@class, 'soggetti-row')]")
-    for row in rows:
-        # print("°"*100)
-        # print(row)
-        cell = row.find_element(By.XPATH, ".//td[2]/span")
-        # print(cell.get_attribute("title"), denominazione)
-        if cell.get_attribute("title") == "SEV SPA":
-            row.click()
-            break
-
-    # Wait for the popup to open
-    time.sleep(3*d_t)
-
-    # Switch to the newly opened modal (if necessary)
-    driver.switch_to.default_content()  # Switch back to default content
-    modal_iframe = driver.find_element(By.CSS_SELECTOR, "iframe[src*='frameindex']")
-    driver.switch_to.frame(modal_iframe)
-
-    time.sleep(2*d_t)
-
-    # Select "Controparte" for both dropdowns
-    person_role_select = driver.find_element(By.ID, "personRole")
-    time.sleep(1 * d_t)
-    person_role_select.send_keys("Cliente")
-
-    #relazione_select = driver.find_element(By.ID, "relazione")
-    #relazione_select.select_by_value('1')
-    select = Select(driver.find_element(By.ID, "relazione"))
-    time.sleep(1 * d_t)
-    select.select_by_value('1')
-
-
-    # Click the "Aggiungi" button
-    add_button = driver.find_element(By.ID, "addPartButton")
-    add_button.click()
-    ####################################################################################################################
     # Wait for the action to complete
-    time.sleep(5*d_t)
+    time.sleep(10)
 
     # Switch back to the default content after the modal interaction
     driver.switch_to.default_content()
@@ -232,7 +146,7 @@ def genera_pratiche(subjects_dict: Dict[str, Any]):
         denominazione = subject_info["Ragione_Sociale"]
 
         pratica = {
-            "tipologia": "11",
+            "tipologia": "1",
             "nome_pratica": "Recupero credito SEV",
             "descrizione": "Recupero credito SEV- Stragiudiziale",
             "categoria": "-1",
@@ -241,7 +155,7 @@ def genera_pratiche(subjects_dict: Dict[str, Any]):
             "stato": "8",
             "data_apertura": subject_info.get("Data affido"),
             "tipo_valore": "0",
-            "valore": subject_info.get("Residuo ad oggi")
+            "valore": str(subject_info.get("Residuo ad oggi")).replace(".", "").replace(",", ".")
         }
 
         pratiche.append((denominazione, pratica))
@@ -267,8 +181,6 @@ def importa_pratiche(subjects_dict: Dict[str, Any]):
 
         # Iterate over each denominazione and dati_pratica pair
         for denominazione, dati_pratica in pratiche:
-            dati_pratica["valore"] = str(dati_pratica["valore"]).replace(".", "").replace(",", ".")
-            print(json.dumps(dati_pratica, indent=2))
             navigate_to_pratiche(driver)
             create_pratica(driver, denominazione, dati_pratica)
 
@@ -308,4 +220,5 @@ if __name__ == "__main__":
         })
     ]
 
-    #importa_pratiche()
+    importa_pratiche()
+
